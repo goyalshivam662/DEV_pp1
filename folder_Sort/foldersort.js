65b78661c406a6646746d532c6bea870caf0e313
shivam goyal <goyalshivam962@gmail.com>
2021-03-07 14:56:22 +0530

    first change

---
+++ folder_Sort/foldersort.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+let folderPath = "./downloads";
+let extent = require("./utility.js");
+let extfolderpath ;
+
+ 
+
+
+function checkfolder(extension){
+// check extension ka ==> already folder exist or not
+//.pdf
+ extfolderpath = folderPath;
+for(let key in extent){
+// Images -> Audio
+if(extent[key].includes(extension)){
+cd
+ //extfolderpath = folderPath +"/"+ key; 
+  // also write using string interpolation
+ extfolderpath = `${folderPath}/${key}`; // ===>   ./downloads/Images
+   break;
+}
+
+}//looop
+return  fs.existsSync(extfolderpath);
+}
+
+
+/// 
+function createfolder(){
+
+fs.mkdirSync(extfolderpath);
+}
+//
+//
+
+function movefile(filename){
+  //copy
+let sourcefilepath = `${folderPath}/${filename}`;
+let destinationpath = `${extfolderpath}/${filename}`;
+fs.copyFileSync(sourcefilepath,destinationpath);
+// delete
+fs.unlinkSync(sourcefilepath);
+
+}
+//
+function sortfolder(folderPath){
+// get content of folder
+ let content = fs.readdirSync(folderPath);
+ for(let i=0 ;i<content.length;i++){
+  // get gextension of each file like abc.pdf => .pdf
+  let extensionname = path.extname(content[i]);
+
+  let isfolder = checkfolder(extensionname);
+  if(isfolder){
+
+    movefile(content[i]);
+
+  }else{
+  
+    createfolder();
+    movefile(content[i]);
+
+  }//else
+ }
+} //
+
+
+sortfolder(folderPath);
---
+++ folder_Sort/utility.js
@@ -0,0 +1,9 @@
+let exts = {
+"Images" : [".png",".jpeg",".gif"],
+"Audio" : [".mp3"],
+"Documents" : [".pdf"],
+"Compressed" : [".zip"],
+"Videos":[".mkv"]
+
+}
+module.exports = exts;
